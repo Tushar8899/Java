dc4592be92759293c7c4208c6ffb7bd8a1aa6f55
Tushar Tyagi <tushartyagi00001@gmail.com>
2025-08-22 16:40:49 +0530

    Update java

---
+++ Type conversion/conversion.java
@@ -0,0 +1,21 @@
+
+public class conversion {
+    public static void main(String[] args)  {
+        // implicit 
+        long big = 23 ;
+        float mid = 2 ;
+        double huge = 3.4f ;
+        System.out.println(mid);
+        // explicit 
+        float eMid = (float) 3.4f ;
+        long eBig = (long) 3.4 ;
+        int eInt = (int) 3.4 ; 
+        System.out.println(eMid);
+        System.out.println(eInt);
+        
+
+
+
+    }
+    
+}
